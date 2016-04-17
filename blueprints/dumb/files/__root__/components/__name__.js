ce13d985f0bcc0752711b13ebb1586e3127c0841
Pablo Sanfilippo <sanfilippopablo@gmail.com>
2016-04-17 17:11:31 -0300

    Updated dumb components blueprint.

---
+++ blueprints/dumb/files/__root__/components/__name__.js
@@ -0,0 +1,12 @@
+import React, { PropTypes } from 'react'
+
+const <%= pascalEntityName %> = ({ props }) => (
+  <div>
+  </div>
+)
+
+<%= pascalEntityName %>.propTypes = {
+  
+}
+
+export default <%= pascalEntityName %>
