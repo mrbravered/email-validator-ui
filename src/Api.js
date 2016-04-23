bd92d8399bcee4b0f831cf0732d6d233992304be
Pablo Sanfilippo <sanfilippopablo@gmail.com>
2016-04-23 02:19:01 -0300

    Added real API call

--- src/Api.js
+++ src/Api.js
@@ -1,19 +1,30 @@
+const INVALID_APIKEY_MESSAGE = 'Invalid APIKey. Logout and login again with a valid API Key.'
+const CONNECTION_ERROR_MESSAGE = 'There\'s seem to be a problem with your connection'
+
+const BASE_URL = 'http://proxy.dirtynagger.com:3000/api/'
+
+// Using real API. Untested (No CORS)
 export function validateEmail (email) {
+  const token = localStorage.getItem('APIKey')
   return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      if (localStorage.getItem('APIKey') === 'asd') {
-        resolve({
-          'emailAddress': email,
-          'status': ['valid', 'invalid', 'unknown'][Math.floor(Math.random() * 3)],
-          'date': new Date()
-        })
-      } else {
-        reject(new Error('Invalid APIKey. Logout and login again with a valid API Key.'))
+    fetch(BASE_URL + `post?emailAddress=${email}`, {
+      mode: 'cors',
+      headers: {
+        'Authorization': `Bearer ${token}`
       }
-    }, 1500)
+    }).then((r) => {
+      r.json().then((data) => {
+        if (data.message) {
+          reject(new Error(INVALID_APIKEY_MESSAGE))
+        } else {
+          resolve(data)
+        }
+      })
+    }).catch((r) => reject(new Error(CONNECTION_ERROR_MESSAGE)))
   })
 }
 
+// Mock function since is not supported by the API yet
 export function validateBulk (emails) {
   return new Promise((resolve, reject) => {
     setTimeout(() => {
@@ -28,7 +39,7 @@ export function validateBulk (emails) {
           })
         })
       } else {
-        reject(new Error('Invalid APIKey.'))
+        reject(new Error(INVALID_APIKEY_MESSAGE))
       }
     }, 50)
   })
