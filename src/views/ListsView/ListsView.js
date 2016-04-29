246148a139868fe8408d41f23893286197598205
Pablo Sanfilippo <sanfilippopablo@gmail.com>
2016-04-29 14:30:57 -0300

    Added Add List button to ListsView

--- src/views/ListsView/ListsView.js
+++ src/views/ListsView/ListsView.js
@@ -1,5 +1,6 @@
 import React, { PropTypes } from 'react'
 import { connect } from 'react-redux'
+import { LinkContainer } from 'react-router-bootstrap'
 import ListsContainer from 'containers/ListsContainer'
 import HeaderWithRightSpinner from 'components/HeaderWithRightSpinner'
 
@@ -16,7 +17,13 @@ export class Lists extends React.Component {
       <div className='container'>
         <div className='row'>
           <div className='col-sm-12 col-md-8 col-md-offset-2 col-lg-6 col-lg-offset-3'>
-            <HeaderWithRightSpinner title='Validation Lists' loading={isFetching} />
+            <HeaderWithRightSpinner title='Validation Lists' loading={isFetching}>
+              <LinkContainer to='/app/lists/new'>
+                <button className='btn btn-primary'>
+                  <i className='fa fa-plus'></i> New list
+                </button>
+              </LinkContainer>
+            </HeaderWithRightSpinner>
             <ListsContainer />
           </div>
         </div>
