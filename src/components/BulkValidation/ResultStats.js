0e71f4a762012f1c55688490aa4d27055f63eeb8
Pablo Sanfilippo <sanfilippopablo@gmail.com>
2016-04-29 01:00:37 -0300

    Updated ResultStats

--- src/components/BulkValidation/ResultStats.js
+++ src/components/BulkValidation/ResultStats.js
@@ -4,16 +4,26 @@ const ResultStats = ({ report }) => {
   const validWidth = report.valid / report.total * 100
   const invalidWidth = report.invalid / report.total * 100
   const unknownWidth = report.unknown / report.total * 100
+  const spamTrapWidth = report.spamtrap / report.total * 100
+  const roleBasedWidth = report['role-based'] / report.total * 100
+  const acceptAllWidth = report['accept all'] / report.total * 100
+
   return (
     <div>
       <div>
         <p><strong>Valid:</strong> {report.valid}</p>
         <p><strong>Invalid:</strong> {report.invalid}</p>
         <p><strong>Unknown:</strong> {report.unknown}</p>
+        <p><strong>Spam Trap:</strong> {report.spamtrap}</p>
+        <p><strong>Role Based:</strong> {report['role-based']}</p>
+        <p><strong>Accept All:</strong> {report['accept all']}</p>
       </div>
       <div className='progress'>
         <div className='progress-bar progress-bar-success' style={{width: validWidth + '%'}}></div>
         <div className='progress-bar progress-bar-info' style={{width: unknownWidth + '%'}}></div>
+        <div className='progress-bar progress-bar-warning' style={{width: spamTrapWidth + '%'}}></div>
+        <div className='progress-bar progress-bar-success' style={{width: roleBasedWidth + '%'}}></div>
+        <div className='progress-bar progress-bar-success' style={{width: acceptAllWidth + '%'}}></div>
         <div className='progress-bar progress-bar-danger' style={{width: invalidWidth + '%'}}></div>
       </div>
     </div>
