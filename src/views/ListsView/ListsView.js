1488104efddf56bbdb59152d1aa84dbeab4f4bca
Pablo Sanfilippo <sanfilippopablo@gmail.com>
2016-04-29 12:05:45 -0300

    Implemented HeaderWithRightSpinner in ListsView

--- src/views/ListsView/ListsView.js
+++ src/views/ListsView/ListsView.js
@@ -1,16 +1,22 @@
-import React from 'react'
+import React, { PropTypes } from 'react'
+import { connect } from 'react-redux'
 import ListsContainer from 'containers/ListsContainer'
+import HeaderWithRightSpinner from 'components/HeaderWithRightSpinner'
 
 export class Lists extends React.Component {
 
+  static propTypes = {
+    isFetching: PropTypes.bool
+  }
+
   render () {
+    const { isFetching } = this.props
+
     return (
       <div className='container'>
         <div className='row'>
           <div className='col-sm-12 col-md-8 col-md-offset-2 col-lg-6 col-lg-offset-3'>
-            <div className='page-header'>
-              <h1>Validation lists</h1>
-            </div>
+            <HeaderWithRightSpinner title='Validation Lists' loading={isFetching} />
             <ListsContainer />
           </div>
         </div>
@@ -19,4 +25,16 @@ export class Lists extends React.Component {
   }
 }
 
-export default Lists
+const mapStateToProps = (state) => {
+  return {
+    isFetching: state.lists.isFetching
+  }
+}
+const mapDispatchToProps = (dispatch) => {
+  return {}
+}
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Lists)
