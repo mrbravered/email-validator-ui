473ea50ef6b82cea6cc90bea56e745bc64f0215f
Pablo Sanfilippo <sanfilippopablo@gmail.com>
2016-04-28 21:29:30 -0300

    Deleted console.log call in listReader

--- src/utils/listReader.js
+++ src/utils/listReader.js
@@ -1,7 +1,6 @@
 export default (file) => new Promise((resolve, reject) => {
   let reader = new FileReader()
   reader.onload = (e) => {
-    console.log(e.target.result)
     resolve(e.target.result)
   }
   reader.onerror = (e) => reject()
