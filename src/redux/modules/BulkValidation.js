99f2b3c180655a2844d451a345ea721df0389a31
Pablo Sanfilippo <sanfilippopablo@gmail.com>
2016-05-03 15:54:56 -0300

    Deleted unused action

--- src/redux/modules/BulkValidation.js
+++ src/redux/modules/BulkValidation.js
@@ -1,5 +1,4 @@
 // Constants
-export const UPDATE_EMAILS_LIST = 'bulk-validation/UPDATE_EMAILS_LIST'
 export const UPLOADING_LIST = 'bulk-validation/UPLOADING_LIST'
 export const UPLOAD_FAILED = 'bulk-validation/UPLOAD_FAILED'
 export const UPLOAD_SUCEEDED = 'bulk-validation/UPLOAD_SUCEEDED'
@@ -7,13 +6,6 @@ export const VALIDATE_REQUESTED = 'bulk-validation/VALIDATE_REQUESTED'
 export const UPDATE_UPLOAD_PROGRESS = 'bulk-validation/UPDATE_UPLOAD_PROGRESS'
 
 // Action Creators
-export function updateEmailsList (emailsList) {
-  return {
-    type: UPDATE_EMAILS_LIST,
-    emailsList: emailsList
-  }
-}
-
 export function validate (emailsArray) {
   return {type: VALIDATE_REQUESTED, emailAddresses: emailsArray}
 }
@@ -21,17 +13,11 @@ export function validate (emailsArray) {
 // Reducer
 export const initialState = {
   uploading: false,
-  emailsList: '',
   error: '',
   uploadProgress: {}
 }
 export default function (state = initialState, action) {
   switch (action.type) {
-    case UPDATE_EMAILS_LIST:
-      return {
-        ...state,
-        emailsList: action.emailsList
-      }
     case UPLOADING_LIST:
       return {
         ...state,
