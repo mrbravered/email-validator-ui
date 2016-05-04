9598a409b85b542de6de4fa56832641f56f4a856
Pablo Sanfilippo <sanfilippopablo@gmail.com>
2016-05-03 23:44:41 -0300

    Main sagas are spawned now instead of forked

--- src/redux/sagas/index.js
+++ src/redux/sagas/index.js
@@ -1,12 +1,12 @@
-import { fork } from 'redux-saga/effects'
+import { spawn } from 'redux-saga/effects'
 import { watchValidate } from './SingleValidation'
 import { watchBulkValidate } from './BulkValidation'
 import listsSaga from './Lists'
 import authSaga from './Auth'
 
 export default function * () {
-  yield fork(watchValidate)
-  yield fork(watchBulkValidate)
-  yield fork(listsSaga)
-  yield fork(authSaga)
+  yield spawn(watchValidate)
+  yield spawn(watchBulkValidate)
+  yield spawn(listsSaga)
+  yield spawn(authSaga)
 }
