1247e4a74cbe873a59933a206262ccf9dc9221b4
chwilldev <cedrichada@gmail.com>
2022-09-17 00:46:42 +0900

    removed: console errors in ui and fixed menu

--- src/forms/NewListForm/NewListForm.js
+++ src/forms/NewListForm/NewListForm.js
@@ -1,4 +1,5 @@
-import React, { PropTypes } from 'react'
+import React from 'react'
+import PropTypes from 'prop-types'
 import { reduxForm } from 'redux-form'
 import ProgressBar from 'components/ProgressBar'
 import FileSelectionInput from 'components/FileSelectionInput'
--- src/parsers/excel/index.js
+++ src/parsers/excel/index.js
@@ -1,4 +1,5 @@
-import React, { PropTypes } from 'react'
+import React from 'react'
+import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
 import * as duck from 'redux/modules/ExcelFileSelector'
 import isEqual from 'lodash/isEqual'
@@ -23,7 +24,7 @@ export const createExcelAdditionalInfo = (file) => {
       this.onColumnSelect = this.onColumnSelect.bind(this)
     }
 
-    componentWillMount () {
+    componentDidMount () {
       this.props.untouch()
     }
 
